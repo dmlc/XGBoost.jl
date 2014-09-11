2655a17bc80cd7fbdb33ec00c97bf0e0a6acdc64
antinucleon <antinucleon@gmail.com>
2014-09-10 23:47:13 -0600

    chg

--- demo/basic_walkthrough.jl
+++ demo/basic_walkthrough.jl
@@ -22,31 +22,31 @@ function readlibsvm(fname::ASCIIString, shape)
 end
 
 # we use auxiliary function to read LIBSVM format into julia Matrix
-train = readlibsvm("../data/agaricus.txt.train", (6513, 126))
-test = readlibsvm("../data/agaricus.txt.test", (1611, 126))
+train_X, train_Y = readlibsvm("../data/agaricus.txt.train", (6513, 126))
+test_X, test_Y = readlibsvm("../data/agaricus.txt.test", (1611, 126))
 
 #-------------Basic Training using XGBoost-----------------
 # note: xgboost naturally handles sparse input
 # use sparse matrix when your feature is sparse(e.g. when you using one-hot encoding vector)
-# model paramters can be set as paramter for ```xgboost``` function, or use an Array{(ASCIIString, Any), 1}/Dict()  
+# model paramters can be set as paramter for ```xgboost``` function, or use an Array{(ASCIIString, Any), 1}/Dict()
 num_round = 2
 
 print("training xgboost with dense matrix\n")
-# you can directly pass julia's matrix or sparse matrix as data, 
+# you can directly pass julia's matrix or sparse matrix as data,
 #   by calling xgboost(data, num_round, label=label, training-parameters)
-bst = xgboost(train[1], num_round, label = train[2], eta=1, max_depth=2, objective="binary:logistic")
+bst = xgboost(train_X, num_round, label = train_Y, eta=1, max_depth=2, objective="binary:logistic")
 
 
 print("training xgboost with sparse matrix\n")
-sptrain = sparse(train[1])
+sptrain = sparse(train_X)
 # alternatively, you can pass parameters in as a map
 param = ["max_depth"=>2, "eta"=>1, "objective"=>"binary:logistic"]
-bst = xgboost(sptrain, num_round, label = train[2], param=param)
+bst = xgboost(sptrain, num_round, label = train_Y, param=param)
 
 # you can also put in xgboost's DMatrix object
 # DMatrix stores label, data and other meta datas needed for advanced features
 print("training xgboost with DMatrix")
-dtrain = DMatrix(train[1], label = train[2])
+dtrain = DMatrix(train_X, label = train_Y)
 bst = xgboost(dtrain, num_round, eta = 1, objective = "binary:logistic")
 
 # you can also specify data as file path to a LibSVM format input
@@ -55,21 +55,21 @@ bst = xgboost("../data/agaricus.txt.train", num_round, max_depth = 2, eta = 1, o
 #--------------------basic prediction using XGBoost--------------
 # you can do prediction using the following line
 # you can put in Matrix, SparseMatrix or DMatrix
-preds = predict(bst, test[1])
-print("test-error=", sum((preds .> 0.5) .!= test[2]) / float(size(preds)[1]), "\n")
+preds = predict(bst, test_X)
+print("test-error=", sum((preds .> 0.5) .!= test_Y) / float(size(preds)[1]), "\n")
 
 #-------------------save and load models-------------------------
 # save model to binary local file
 save(bst, "xgb.model")
 # load binary model to julia
 bst2 = Booster(model_file = "xgb.model")
-preds2 = predict(bst2, test[1])
+preds2 = predict(bst2, test_X)
 print("sum(abs(pred2-pred))=", sum(abs(preds2 .- preds)), "\n")
 
 #----------------Advanced features --------------
 # to use advanced features, we need to put data in xgb.DMatrix
-dtrain = DMatrix(train[1], label = train[2])
-dtest = DMatrix(test[1], label = test[2])
+dtrain = DMatrix(train_X, label = train_Y)
+dtest = DMatrix(test_X, label = test_Y)
 
 #---------------Using watchlist----------------
 # watchlist is a list of DMatrix, each of them tagged with name
