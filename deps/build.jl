33a8eaa4d4e8a7d4df1fc8d694a6a730eb0773df
Scott Lundberg <scottmlundberg@gmail.com>
2016-01-21 13:07:15 -0800

    Now locked to xgboost version 0.47 to fix builds.

--- deps/build.jl
+++ deps/build.jl
@@ -7,13 +7,13 @@ prefix=joinpath(BinDeps.depsdir(libxgboostwrapper),"usr")
 provides(BuildProcess,
            (@build_steps begin
                `rm -rf xgboost`
-               `git clone https://github.com/tqchen/xgboost.git`
+               `git clone https://github.com/dmlc/xgboost.git --recursive`
                CreateDirectory(prefix)
                CreateDirectory(joinpath(prefix, "lib"))
                @build_steps begin
                    ChangeDirectory("xgboost")
                    FileRule(joinpath(prefix,"lib","libxgboostwrapper.so"), @build_steps begin
-                       `git checkout master` # v0.40
+                       `git checkout 0.47` # v0.40
                        `bash build.sh`
                        `cp wrapper/libxgboostwrapper.so $prefix/lib`
                    end)
