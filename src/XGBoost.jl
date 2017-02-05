44a3887c23a232efa1ac05314b3161cf71c05758
Allardvm <a.vanmossel@gmail.com>
2017-02-05 16:43:10 +0100

    Export new functionality

--- src/XGBoost.jl
+++ src/XGBoost.jl
@@ -6,7 +6,14 @@ include("libxgboost.jl")
 include("data.jl")
 include("learning.jl")
 
-export DMatrix, Booster
-export xgboost, predict, save, nfold_cv, slice, get_info, set_info, dump_model, importance
+export DMatrix
+export get_base_margin, get_float_info, get_label, get_uint_info, get_weight, num_col, num_row,
+    save_binary, set_base_margin, set_float_info, set_group, set_label, set_uint_info, set_weight
+
+export Booster
+export attr, attributes, boost, dump_model, eval, eval_set, get_dump, load_model, predict,
+    save_model, set_attr, set_param
+
+export xgboost, predict, nfold_cv, slice, dump_model, importance
 
 end # module XGBoost
