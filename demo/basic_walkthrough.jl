00ba260832ae7be9654d6754551e064c66668681
Andreas Noack <andreasnoackjensen@gmail.com>
2017-11-22 17:09:53 -0500

    Make base_walkthrough.jl demo work on Julia 0.6
    Don't assume that demo is run from demo directory

--- demo/basic_walkthrough.jl
+++ demo/basic_walkthrough.jl
@@ -1,8 +1,10 @@
 using XGBoost
 
+const DATAPATH = joinpath(dirname(@__FILE__()), "..", "data")
+
 # we load in the agaricus dataset
 # In this example, we are aiming to predict whether a mushroom can be eated
-function readlibsvm(fname::ASCIIString, shape)
+function readlibsvm(fname::String, shape)
     dmx = zeros(Float32, shape)
     label = Float32[]
     fi = open(fname, "r")
@@ -13,7 +15,7 @@ function readlibsvm(fname::ASCIIString, shape)
         line = line[2:end]
         for itm in line
             itm = split(itm, ":")
-            dmx[cnt, int(itm[1]) + 1] = float(int(itm[2]))
+            dmx[cnt, parse(Int, itm[1]) + 1] = parse(Int, itm[2])
         end
         cnt += 1
     end
@@ -22,8 +24,8 @@ function readlibsvm(fname::ASCIIString, shape)
 end
 
 # we use auxiliary function to read LIBSVM format into julia Matrix
-train_X, train_Y = readlibsvm("../data/agaricus.txt.train", (6513, 126))
-test_X, test_Y = readlibsvm("../data/agaricus.txt.test", (1611, 126))
+train_X, train_Y = readlibsvm(joinpath(DATAPATH, "agaricus.txt.train"), (6513, 126))
+test_X, test_Y   = readlibsvm(joinpath(DATAPATH, "agaricus.txt.test"), (1611, 126))
 
 #-------------Basic Training using XGBoost-----------------
 # note: xgboost naturally handles sparse input
@@ -53,7 +55,7 @@ dtrain = DMatrix(train_X, label = train_Y)
 bst = xgboost(dtrain, num_round, eta = 1, objective = "binary:logistic")
 
 # you can also specify data as file path to a LibSVM format input
-bst = xgboost("../data/agaricus.txt.train", num_round, max_depth = 2, eta = 1,
+bst = xgboost(joinpath(DATAPATH, "agaricus.txt.train"), num_round, max_depth = 2, eta = 1,
               objective = "binary:logistic")
 
 #--------------------basic prediction using XGBoost--------------
@@ -68,7 +70,7 @@ save(bst, "xgb.model")
 # load binary model to julia
 bst2 = Booster(model_file = "xgb.model")
 preds2 = predict(bst2, test_X)
-print("sum(abs(pred2-pred))=", sum(abs(preds2 .- preds)), "\n")
+print("sum(abs(pred2-pred))=", sum(abs, preds2 .- preds), "\n")
 
 #----------------Advanced features --------------
 # to use advanced features, we need to put data in xgb.DMatrix
@@ -100,4 +102,4 @@ print("test-error=", sum((pred .> 0.5) .!= label) / float(size(pred)[1]), "\n")
 # Finally, you can dump the tree you learned using dump_model into a text file
 dump_model(bst, "dump.raw.txt")
 # If you have feature map file, you can dump the model in a more readable way
-dump_model(bst, "dump.nice.txt", fmap = "../data/featmap.txt")
+dump_model(bst, "dump.nice.txt", fmap = joinpath(DATAPATH, "featmap.txt"))
