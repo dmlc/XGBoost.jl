035794d5a2c6dd4f249423755e06e0132b6d088c
Avik Sengupta <avik@sengupta.net>
2020-04-04 11:28:36 +0100

    remove old binarybuilder deps file

--- src/XGBoost.jl
+++ src/XGBoost.jl
@@ -10,11 +10,7 @@ export xgboost, predict, save, nfold_cv, slice, get_info, set_info, dump_model, 
 export rabit_init, rabit_finalize, rabit_is_distributed, rabit_get_rank, rabit_get_world_size, rabit_get_version_number
 
 global const build_version = "0.82"
-include("../deps/deps.jl")
 
-function __init__()
-    check_deps()
-end
 include("xgboost_wrapper_h.jl")
 include("rabit_wrapper.jl")
 include("xgboost_lib.jl")
